9564a25fdab810bef6084a54e83ded830a1d17bf
Joshua Anthony <jantho1990@gmail.com>
2019-04-29 12:34:01 -0500

    added backgrounds for main menu; added randomizer to select which background to display for main menu when it is accessed

--- screens/MainMenu.gd
+++ screens/MainMenu.gd
@@ -7,13 +7,23 @@ extends MarginContainer
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
+	random_background()
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 
+func random_background():
+	var num = 3 # count of backgrounds minus 1
+	var bkgd = math.rand(0, num)
+	var i = 0
+	for child in $BackgroundTiles.get_children():
+		if i == bkgd:
+			child.show()
+		else:
+			child.hide()
+		i += 1
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
